dad8bb797f71b0b6bdaf359acf6b06bd733349e4
EduardoAyora <eduardoaayora64@gmail.com>
2023-04-25 22:37:43 -0500

    Reconexión automática

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const messagesDB = []
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.emit('load all', messagesDB);
   socket.on('chat message', (msg) => {
+    messagesDB.push(msg)
     io.emit('chat message', msg);
   });
   socket.on('disconnect', () => {
