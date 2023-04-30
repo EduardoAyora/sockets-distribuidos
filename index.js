e3bd568df8fd9a9a00de295470f1c9bb2fda1581
EduardoAyora <eduardoaayora64@gmail.com>
2023-04-30 18:42:50 -0500

    hash

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require("socket.io");
-const cors = require('cors');
-const app = express();
-app.use(cors({
-  origin: '*'
-}));
-const server = http.createServer(app);
-const io = new Server(server, {cors: {
-  origin: "*",
-  methods: ["GET", "POST"]
-}});
+const express = require('express')
+const { createHmac } = require('crypto')
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors')
+const app = express()
+app.use(
+  cors({
+    origin: '*',
+  })
+)
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+})
 
+const secret = 'claveS3cR3t4'
 const messagesDB = []
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html')
+})
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.emit('load all', messagesDB);
-  socket.on('chat message', (msg) => {
-    messagesDB.push(msg)
-    io.emit('chat message', msg);
-  });
+  console.log('a user connected')
+  socket.emit('load all', messagesDB)
+  socket.on('chat message', ({ message, hash }) => {
+    const serverHash = createHmac('sha256', secret)
+      .update(message)
+      .digest('hex')
+
+    if (serverHash != hash) return
+    messagesDB.push(message)
+    io.emit('chat message', message)
+  })
   socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
+    console.log('user disconnected')
+  })
+})
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+  console.log('listening on *:3000')
+})
